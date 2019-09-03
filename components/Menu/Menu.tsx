58f0217951d0983070a5f12de3177a0e274b1930
David B <babel.david@gmail.com>
2019-09-03 13:16:10 +0200

    manage ssr for material ui

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {
-  Drawer,
   ListItem,
   ListItemText,
   ListItemIcon,
@@ -21,89 +20,86 @@ export function Menu({ width }: Props) {
         {/* <MenuIcon onClick={() => setOpen(true)} /> */}
       </div>
 
-      <Drawer variant="permanent" anchor="left">
-        <div
-          style={{ position: 'fixed', top: 5, left: 5, zIndex: 888888888888 }}
+      <div style={{ position: 'fixed', top: 5, left: 5, zIndex: 888888888888 }}>
+        {/* <CloseMenuIcon onClick={() => setOpen(false)} /> */}
+      </div>
+      <List style={{ width: width }}>
+        <h2 style={{ textAlign: 'center', marginTop: 55 }}>Update infos</h2>
+        <ListItem
+          button
+          onClick={() => {
+            // setOpen(false);
+            // setModalInfoKillVisibility(true);
+          }}
         >
-          {/* <CloseMenuIcon onClick={() => setOpen(false)} /> */}
-        </div>
-        <List style={{ width: width }}>
-          <h2 style={{ textAlign: 'center', marginTop: 55 }}>Update infos</h2>
-          <ListItem
-            button
-            onClick={() => {
-              // setOpen(false);
-              // setModalInfoKillVisibility(true);
-            }}
-          >
-            <ListItemIcon>{/* <GridPlus /> */}</ListItemIcon>
-            <ListItemText primary="Add a boss kill ?" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem
-            button
-            onClick={() => {
-              // setOpen(false);
-              // setModalCreateGuildVisibility(true);
-            }}
-          >
-            <ListItemIcon>{/* <AddGuild /> */}</ListItemIcon>
-            <ListItemText primary="Add your guild" />
-          </ListItem>
-          <ListItem
-            button
-            onClick={() => {
-              // setOpen(false);
-              // setModalCreateServerVisibility(true);
-            }}
-          >
-            <ListItemIcon>{/* <AddServer /> */}</ListItemIcon>
-            <ListItemText primary="Add your Server" />
-          </ListItem>
-        </List>
-        <Divider />
-        <List>
-          <ListItem
-            button
-            onClick={() => {
-              // setOpen(false);
-              // setModalHelpVisibility(true);
-            }}
-          >
-            <ListItemIcon>{/* <HelpIcon /> */}</ListItemIcon>
-            <ListItemText primary="Help" />
-          </ListItem>
-          <ListItem
-            button
-            onClick={() => {
-              // setOpen(false);
-              // window.open('https://www.paypal.me/devilhunter/2', '_blank');
-            }}
-          >
-            <ListItemIcon>{/* <CoinIcon /> */}</ListItemIcon>
-            {/* <ListItemIcon>
+          <ListItemIcon>{/* <GridPlus /> */}</ListItemIcon>
+          <ListItemText primary="Add a boss kill ?" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem
+          button
+          onClick={() => {
+            // setOpen(false);
+            // setModalCreateGuildVisibility(true);
+          }}
+        >
+          <ListItemIcon>{/* <AddGuild /> */}</ListItemIcon>
+          <ListItemText primary="Add your guild" />
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => {
+            // setOpen(false);
+            // setModalCreateServerVisibility(true);
+          }}
+        >
+          <ListItemIcon>{/* <AddServer /> */}</ListItemIcon>
+          <ListItemText primary="Add your Server" />
+        </ListItem>
+      </List>
+      <Divider />
+      <List>
+        <ListItem
+          button
+          onClick={() => {
+            // setOpen(false);
+            // setModalHelpVisibility(true);
+          }}
+        >
+          <ListItemIcon>{/* <HelpIcon /> */}</ListItemIcon>
+          <ListItemText primary="Help" />
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => {
+            // setOpen(false);
+            // window.open('https://www.paypal.me/devilhunter/2', '_blank');
+          }}
+        >
+          <ListItemIcon>{/* <CoinIcon /> */}</ListItemIcon>
+          {/* <ListItemIcon>
               <BitcoinIcon />
             </ListItemIcon> */}
-            <ListItemText primary="Thank me with a 🍺" />
-          </ListItem>
-          <ListItem
-            button
-            onClick={() => {
-              // setOpen(false);
-              // window.open(
-              //   'https://github.com/DavidBabel/wow-classic-pve/issues/new/choose',
-              //   '_blank'
-              // );
-            }}
-          >
-            <ListItemIcon>{/* <BugIcon /> */}</ListItemIcon>
-            <ListItemText primary="Report a bug" />
-          </ListItem>
-          {/* <AppVersion width={listWidth} /> */}
-        </List>
-      </Drawer>
+          <ListItemText primary="Thank me with a 🍺" />
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => {
+            // setOpen(false);
+            // window.open(
+            //   'https://github.com/DavidBabel/wow-classic-pve/issues/new/choose',
+            //   '_blank'
+            // );
+          }}
+        >
+          <ListItemIcon>{/* <BugIcon /> */}</ListItemIcon>
+          <ListItemText primary="Report a bug" />
+        </ListItem>
+        {/* <AppVersion width={listWidth} /> */}
+      </List>
+
       {/* <ModalCreateServer
         isOpen={isModalCreateServerVisible}
         onClose={() => setModalCreateServerVisibility(false)}
--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,20 @@ import App, { Container } from 'next/app';
 import React from 'react';
 import withApolloClient from '../lib/apollo-setup/with-apollo-client';
 import { ApolloProvider } from '@apollo/react-hooks';
-import { Header } from '../components/page/Header';
 import { Menu } from '../components/Menu/Menu';
 
 class AppWithApollo extends App {
   render() {
     const { Component, pageProps, apolloClient } = this.props;
+    console.log(this.props);
     return (
       <Container>
         <ApolloProvider client={apolloClient}>
-          <Header />
-          <Menu />
-          <div style={{ marginLeft: 300 }}>
-            <Component {...pageProps} />
+          <div style={{ display: 'flex' }}>
+            <Menu />
+            <div>
+              <Component {...pageProps} />
+            </div>
           </div>
         </ApolloProvider>
       </Container>
