d7297fa31081c08a5f4b493aed93942b88999736
David B <babel.david@gmail.com>
2019-09-26 14:26:27 +0200

    wip

--- db/gen/db.sql
+++ db/gen/db.sql
@@ -1305,12 +1305,12 @@ VALUES
  -- SAFE BEGIN FOR CONCAT
 
 
-INSERT INTO "ClassItem" ("itemValueForThisClass","itemId","classId")
-VALUES
-(2, (SELECT id FROM "Items" WHERE "wowheadId"=16863), (SELECT id FROM "Classes" WHERE "name"='Chasseur')),
-(5, (SELECT id FROM "Items" WHERE "wowheadId"=16863), (SELECT id FROM "Classes" WHERE "name"='Guerrier Tank'));
+-- INSERT INTO "ClassItem" ("itemValueForThisClass","itemId","classId")
+-- VALUES
+-- (2, (SELECT id FROM "Items" WHERE "wowheadId"=16863), (SELECT id FROM "Classes" WHERE "name"='Chasseur')),
+-- (5, (SELECT id FROM "Items" WHERE "wowheadId"=16863), (SELECT id FROM "Classes" WHERE "name"='Guerrier Tank'));
 
-INSERT INTO "Loots" ("playerId","itemId","raidId")
-VALUES
-((SELECT id FROM "Players" WHERE "name"='Devilhunter'), (SELECT id FROM "Items" WHERE "wowheadId"=16863), 1),
-((SELECT id FROM "Players" WHERE "name"='Devilhunter'), (SELECT id FROM "Items" WHERE "wowheadId"=16863), 1);
+-- INSERT INTO "Loots" ("playerId","itemId","raidId")
+-- VALUES
+-- ((SELECT id FROM "Players" WHERE "name"='Devilhunter'), (SELECT id FROM "Items" WHERE "wowheadId"=16863), 1),
+-- ((SELECT id FROM "Players" WHERE "name"='Devilhunter'), (SELECT id FROM "Items" WHERE "wowheadId"=16863), 1);
