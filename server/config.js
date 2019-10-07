092cc20f8493ce57fb51f33003a7a14ab5efb201
David B <babel.david@gmail.com>
2019-10-07 17:57:35 +0200

    helper connexion string

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 module.exports = {
   COOKIE_LIFE: 60 * 60 * 12, // 12h
+  // postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
   DATABASE_URL: process.env.DATABASE_URL || "postgres://localhost:5432/test",
   DATE_FORMAT: "yyyy-MM-dd",
   GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT || "api/queries",
