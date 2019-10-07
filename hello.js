39fb95fd3e97ccd5f50921573ae03bc7ff42d6dc
David B <babel.david@gmail.com>
2019-10-07 14:22:06 +0200

    deploy hello world

---
+++ hello.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.send({ v: 1, message: "hello world" });
+});
+
+http.createServer(app).listen(process.env.PORT || 8000);
