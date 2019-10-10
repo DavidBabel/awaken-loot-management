68d76ba112a6b0a148b3e08fe385c83fcbec3d7f
David B <babel.david@gmail.com>
2019-10-10 11:16:29 +0200

    debug env vars

--- apollo.config.js
+++ apollo.config.js
@@ -1,12 +1,12 @@
-const CONFIG = require('./server/config');
+const CONFIG = require("./server/config");
 
 module.exports = {
   client: {
-    name: 'Awaken Loot management',
-    includes: ['pages/**', 'lib/**'],
+    name: "Awaken Loot management",
+    includes: ["pages/**", "lib/**"],
     service: {
-      name: 'API',
-      url: CONFIG.SERVER_URL + '/' + CONFIG.GRAPHQL_ENDPOINT,
+      name: "API",
+      url: CONFIG.SERVER_URL + "/" + CONFIG.GRAPHQL_ENDPOINT,
       // optional disable SSL validation check
       skipSSLValidation: true
     }
--- server/config.js
+++ server/config.js
@@ -1,3 +1,11 @@
+// tslint:disable-next-line:no-console
+console.debug({
+  DATABASE_URL: process.env.DATABASE_URL,
+  GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+  JWT_SECRET: process.env.JWT_SECRET,
+  SERVER_URL: process.env.SERVER_URL
+});
+
 module.exports = {
   COOKIE_LIFE: 60 * 60 * 12, // 12h
   // postgresql://[user[:password]@][netloc][:port][/dbname][?param1=value1&...]
