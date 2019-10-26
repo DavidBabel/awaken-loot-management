3c3736d547b15cb300a45c9b508dc26d28e5570e
David B <babel.david@gmail.com>
2019-10-26 03:13:45 +0200

    allow to update static content and keep prod datas

--- bin/db/backup-prod.sh
+++ bin/db/backup-prod.sh
@@ -2,18 +2,27 @@
 
 # $PROD_DB is a local env var
 
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 date=$(date '+%Y-%m-%d-%H-%M-%S')
 mkdir "./db/backups/$date"
-echo Backup All ...
+echo -e $RED Backup All ... $NC
 pg_dump $PROD_DB > "./db/backups/$date/db-all.sql"
-echo Backup Players ...
-pg_dump $PROD_DB -t "public.\"Players\"" > "./db/backups/$date/db-table-player.sql"
-echo Backup PlayerMerit ...
-pg_dump $PROD_DB -t "public.\"PlayerMerit\"" > "./db/backups/$date/db-table-playermerit.sql"
-echo Backup Loots ...
-pg_dump $PROD_DB -t "public.\"Loots\"" > "./db/backups/$date/db-table-loots.sql"
-echo Backup Raids ...
-pg_dump $PROD_DB -t "public.\"Raids\"" > "./db/backups/$date/db-table-raids.sql"
-echo Backup RaidPlayers ...
-pg_dump $PROD_DB -t "public.\"RaidPlayers\"" > "./db/backups/$date/db-table-raidplayers.sql"
-echo Backup finished
+
+echo -e $RED Backup Players ... $NC
+pg_dump $PROD_DB --data-only -t "public.\"Players\"" > "./db/backups/$date/db-table-player.sql"
+
+echo -e $RED Backup PlayerMerit ... $NC
+pg_dump $PROD_DB --data-only -t "public.\"PlayerMerit\"" > "./db/backups/$date/db-table-playermerit.sql"
+
+echo -e $RED Backup Loots ... $NC
+pg_dump $PROD_DB --data-only -t "public.\"Loots\"" > "./db/backups/$date/db-table-loots.sql"
+
+echo -e $RED Backup Raids ... $NC
+pg_dump $PROD_DB --data-only -t "public.\"Raids\"" > "./db/backups/$date/db-table-raids.sql"
+
+echo -e $RED Backup RaidPlayers ... $NC
+pg_dump $PROD_DB --data-only -t "public.\"RaidPlayers\"" > "./db/backups/$date/db-table-raidplayers.sql"
+
+echo -e $RED Backup finished $NC
--- bin/db/renew-static-tables.sh
+++ bin/db/renew-static-tables.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 
-# ./bin/db/backup-prod.sh
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+date=$(date '+%Y-%m-%d-%H-%M-%S')
+./bin/db/backup-prod.sh
+
+echo -e $RED Reset all databases ... $NC
 psql $PROD_DB < ./bin/db/reset_static_databases.sql
 # psql $PROD_DB < ./db/gen/db.sql
-# psql $PROD_DB < ./bin/db/trucate_tables.sql
+# # local management
+echo -e $RED Create Db structure ... $NC
+psql $PROD_DB < ./db/01-create.sql
+
+echo -e $RED Create data classes and slots ... $NC
+psql $PROD_DB < ./db/05-class_and_slot.sql
+
+echo -e $RED Create data bosses ... $NC
+psql $PROD_DB < ./db/08-data_bosses.sql
+
+echo -e $RED Create data boss items ... $NC
+psql $PROD_DB < ./db/11-data_boss_items.sql
+
+echo -e $RED Create data merites ... $NC
+psql $PROD_DB < ./db/40-merites.sql
+
+echo -e $RED Create data players ... $NC
+psql $PROD_DB < ./db/70-seeds_players.sql
+
+# # remote management
+echo -e $RED Import player merits $NC
+psql $PROD_DB < ./db/backups/$date/db-table-playermerit.sql
+
+echo -e $RED Import raids ... $NC
+psql $PROD_DB < ./db/backups/$date/db-table-raids.sql
+
+echo -e $RED Import raid players ... $NC
+psql $PROD_DB < ./db/backups/$date/db-table-raidplayers.sql
+
+echo -e $RED Import loots ... $NC
+psql $PROD_DB < ./db/backups/$date/db-table-loots.sql
