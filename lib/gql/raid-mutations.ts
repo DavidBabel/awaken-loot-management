d3f86713cb4ba4f91e5b77145566e9b9e9c574bf
David B <babel.david@gmail.com>
2019-11-04 16:46:05 +0100

    wip

--- lib/gql/raid-mutations.ts
+++ lib/gql/raid-mutations.ts
@@ -9,3 +9,17 @@ export const CREATE_RAID = gql`
     }
   }
 `;
+
+// export const CREATE_RAID = gql`
+//   mutation CreateRaid($date: String, $donjonId: Int!) {
+//     createRaid(input: { raid: { date: $date, donjonId: $donjonId } }) {
+//       raid {
+//         id
+//       }
+//     }
+//   }
+// `;
+
+// mutation MyMutation($raidId: Int!, $playerId: Int!) {
+//   createRaidPlayer(input: {raidPlayer: {playerId: $playerId, raidId: $raidId}})
+// }
