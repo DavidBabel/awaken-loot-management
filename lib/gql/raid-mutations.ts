6b2ea80892363eece0059ca0089fdf411aa3e20b
David B <babel.david@gmail.com>
2019-11-04 16:08:18 +0100

    fix create raid

--- lib/gql/raid-mutations.ts
+++ lib/gql/raid-mutations.ts
@@ -1,7 +1,7 @@
 import { gql } from "apollo-boost";
 
 export const CREATE_RAID = gql`
-  mutation CreateRaid($date: String, $donjonId: Int) {
+  mutation CreateRaid($date: String, $donjonId: Int!) {
     createRaid(input: { raid: { date: $date, donjonId: $donjonId } }) {
       raid {
         id
